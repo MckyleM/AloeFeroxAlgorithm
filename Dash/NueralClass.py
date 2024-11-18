2f1469341791f59c6df394707913158a6e10b010
MichaelPautz28 <michaelpautz03@gmail.com>
2024-11-18 12:10:52 +0200

    changes

--- Dash/NueralClass.py
+++ Dash/NueralClass.py
@@ -3,7 +3,7 @@ class SingleLabelCNN(nn.Module):
     def __init__(self, num_classes):
         super(SingleLabelCNN, self).__init__()
         self.base_model = models.resnet18(pretrained=True)
-        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, num_classes)  # Adjust for single label output
+        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, num_classes)
 
     def forward(self, x):
         x = self.base_model(x)
